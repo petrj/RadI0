b9fd3d0e2b98639b17e421ba455c5ba18656f622
petrj <petrjanousek.net@gmail.com>
2026-03-18 17:57:00 +0100

    removed Console project

--- Clear.ps1
+++ Clear.ps1
@@ -3,31 +3,26 @@ cd $PSScriptRoot
 
 foreach ($folder in `
     @(
-    "packages",
-    "LoggerService\bin",
-    "LoggerService\obj",
     "RadI0\bin",
     "RadI0\obj",
     "RTLSDR\bin",
     "RTLSDR\obj",
     "RTLSDR.Audio\bin",
     "RTLSDR.Audio\obj",
-    "RTLSDR.Audio.Console\bin",
-    "RTLSDR.Audio.Console\obj",
     "RTLSDR.Common\bin",
     "RTLSDR.Common\obj",
     "RTLSDR.FM\bin",
     "RTLSDR.FM\obj",
     "RTLSDR.DAB\bin",
     "RTLSDR.DAB\obj",
-    "RTLSDR.FMDAB.Console\bin",
-    "RTLSDR.FMDAB.Console\obj",
     "Tests\bin",
     "Tests\obj",
     ".vs"
      ))
 {
     $fullPath = [System.IO.Path]::Combine($scriptPath,$folder)
+
+    Write-Host "Clearing $folder..."
 
     if (-not $fullPath.EndsWith("\"))
     {
